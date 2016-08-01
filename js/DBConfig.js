150432dfc86766ae78c10694885f98c56a2a33c0
Luis Alcantar <XZBQ38@NAM.corp.gm.com>
2016-08-01 16:59:46 -0700

    removing dependencies

--- js/DBConfig.js
+++ js/DBConfig.js
@@ -1,18 +1,22 @@
-  var firebase = require(vendor/firebase.js);
-  var config = {
-    apiKey: "AIzaSyAeUUth3tU5auemQujKCmxRE3VS-y4QQxE",
-    authDomain: "vacationtracker-5d242.firebaseapp.com",
-    databaseURL: "https://vacationtracker-5d242.firebaseio.com",
-    storageBucket: "",
-  };
+ var firebase = require(vendor/firebase.js);
+ var config = {
+ 	apiKey: "AIzaSyAeUUth3tU5auemQujKCmxRE3VS-y4QQxE",
+ 	authDomain: "vacationtracker-5d242.firebaseapp.com",
+ 	databaseURL: "https://vacationtracker-5d242.firebaseio.com",
+ 	storageBucket: "",
+ };
 
-  firebase.initializeApp(config);
+ firebase.initializeApp(config);
 
-/*Test Connection*/
-  firebase.child('.info/connected').on('value', function(connectedSnap) {
-  if (connectedSnap.val() === true) {
-  		console.log("ON");
-    } else {
-  		console.log("off");
-  }
-});
+
+
+ /*Test Connection*/
+ var firebaseRef = new Firebase('http://INSTANCE.firebaseio.com');
+
+ firebaseRef.child('.info/connected').on('value', function(connectedSnap) {
+ 	if (connectedSnap.val() === true) {
+ 		console.log("ON");
+ 	} else {
+ 		console.log("off");
+ 	}
+ });
