7a9515e2ceef7a567d2f3d9be17f62074a6084c6
Luis Alcantar <XZBQ38@NAM.corp.gm.com>
2016-08-01 14:41:34 -0700

    add require, for the firebase

--- js/DBConfig.js
+++ js/DBConfig.js
@@ -1,10 +1,9 @@
-<script>
-  // Initialize Firebase
+  var firebase = require(vendor/firebase.js);
   var config = {
     apiKey: "AIzaSyAeUUth3tU5auemQujKCmxRE3VS-y4QQxE",
     authDomain: "vacationtracker-5d242.firebaseapp.com",
     databaseURL: "https://vacationtracker-5d242.firebaseio.com",
     storageBucket: "",
   };
+  
   firebase.initializeApp(config);
-</script>
