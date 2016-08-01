0fd8cc8004386f81f8da31704f1a8927239770ac
Luis Valenzuela <FZ3T8L@NAM.corp.gm.com>
2016-08-01 15:36:54 -0700

    Moved inline js in index.html and login.html to calendarcontroller.js and logincontroller.js respectively.

---
+++ js/logincontroller.js
@@ -0,0 +1,38 @@
+$(document).foundation();
+
+function Account(GMIN, First, Last, Email, Manager, TotalVacation, UsedVacation) {
+    this.GMIN = GMIN;
+    this.First = First;
+    this.Last = Last;
+    this.Email = Email;
+    this.Manager = Manager;
+    this.TotalVacation = TotalVacation;
+    this.UsedVacation = UsedVacation;
+};
+var robert = Account("242348733", "Robert", "Kasper", "robert.kasperiv@gm.com", "Michael Eilers", "15", "3");
+
+var steven = Account("546461651", "Steven", "Angus", "steven.angus@gm.com", "Michael Eilers", "15", "0");
+	
+$(document).ready(function() {
+    var popup = new Foundation.Reveal($('#createProfileModal'));
+    $("#newUserBtn").click(function () {
+        popup.open();
+    });
+    $("#createCloseBtn").click(function () {
+        var ans = confirm("Are you sure you want to cancel? All progress will be lost.");
+      	if (ans == true)
+            popup.close();
+		});
+    $("#createBtn").click(function () {
+        var account = new Account($("#GMIN").val(), $("#firstName").val(), $("#lastName").val(),
+                                  $("#emailAddress").val(), "Michael Eilers", $("#vacationDaysTotal").val(), $("#vacationDaysUsed").val());
+        popup.close();
+    });
+});
+
+function setData(){
+    var select = document.getElementById('gmin');
+    var gmin = select.options[select.selectedIndex].value;
+    document.accountSelection.action = "index.html?gmid=" + gmin;
+    accountSelection.submit();
+};
