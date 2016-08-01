b76616208985a38aae4854d4ff1cb3d5d8b18e9b
amoawad12 <amoawad@asu.edu>
2016-08-01 14:32:59 -0700

    adding DB config

---
+++ DBConfig.js
@@ -0,0 +1,10 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAeUUth3tU5auemQujKCmxRE3VS-y4QQxE",
+    authDomain: "vacationtracker-5d242.firebaseapp.com",
+    databaseURL: "https://vacationtracker-5d242.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config);
+</script>
