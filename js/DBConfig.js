c2e0c29b3ca358c3b093d4b0a111058b09563d8a
Luis Alcantar <XZBQ38@NAM.corp.gm.com>
2016-08-02 13:52:09 -0700

    diagrams 2.0

--- js/DBConfig.js
+++ js/DBConfig.js
@@ -1,17 +1,33 @@
  // Initialize Firebase
  var config = {
-    apiKey: "AIzaSyAeUUth3tU5auemQujKCmxRE3VS-y4QQxE",
-    authDomain: "vacationtracker-5d242.firebaseapp.com",
-    databaseURL: "https://vacationtracker-5d242.firebaseio.com",
-    storageBucket: "vacationtracker-5d242.appspot.com",
-  };
+ 	apiKey: "AIzaSyAeUUth3tU5auemQujKCmxRE3VS-y4QQxE",
+ 	authDomain: "vacationtracker-5d242.firebaseapp.com",
+ 	databaseURL: "https://vacationtracker-5d242.firebaseio.com",
+ 	storageBucket: "vacationtracker-5d242.appspot.com",
+ };
  firebase.initializeApp(config);
 
-
- /*Get reference*/
+ /*Get reference example=*/
  var value;
  var dbRef = firebase.database().ref().child('employee');
+ dbRef.on('value', function(snapshot) {
+ 	console.log(snapshot.val());
+ });
 
-dbRef.on('value', function(snapshot) {
-  console.log(snapshot.val());
-});
+
+
+ /**
+  * getEmployeeCount(), this method returns the
+  * total number of employees in the database 
+  */
+ int getEmployeeCount() {
+ 	return 0;
+ }
+
+ /**
+  * getManagerCount(), this method returns the
+  * total number of managers in the database 
+  */
+ int getManagerCount() {
+ 	return 0;
+ }
