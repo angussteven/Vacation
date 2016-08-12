591dcfe2531b3dd456942147b3f4d68e017804ef
Nathan Kern <BZYJ7R@NAM.corp.gm.com>
2016-08-12 10:25:51 -0700

    fixed bug when closing create profile where it would throw a bunch of focus errors

--- js/logincontroller.js
+++ js/logincontroller.js
@@ -2,17 +2,48 @@
 $(document).ready(function () {
     var popup = new Foundation.Reveal($('#createProfileModal'));
     $("#createCloseBtn").click(function () {
+        toggleRequired();
             popup.close();
+            
     });
 
     $(document).foundation();
     $("#createProfileForm").on("submit", function(ev) {
         ev.preventDefault();
         createProfile();
+        toggleRequired();
         $('#createProfileModal').foundation('close');
   });
 });
-  
+
+function toggleRequired(){
+    if(document.getElementById('profileFirstName').required == true){
+        document.getElementById('profileFirstName').required = false;
+        document.getElementById('profileLastName').required = false;
+        document.getElementById('emailAddress').required = false;
+        document.getElementById('emailAddressVerify').required = false;
+        document.getElementById('profilePassword').required = false;
+        document.getElementById('profilePasswordVerify').required = false;
+        document.getElementById('noMan').required = false;
+        document.getElementById('selectedManager').required = false;
+        document.getElementById('selectedTeam').required = false;
+        document.getElementById('vacationDaysTotal').required = false;
+        document.getElementById('vacationDaysLeft').required = false;
+    }else{
+       document.getElementById('profileFirstName').required = true;
+        document.getElementById('profileLastName').required = true;
+        document.getElementById('emailAddress').required = true;
+        document.getElementById('emailAddressVerify').required = true;
+        document.getElementById('profilePassword').required = true;
+        document.getElementById('profilePasswordVerify').required = true;
+        document.getElementById('noMan').required = true;
+        document.getElementById('selectedManager').required = true;
+        document.getElementById('selectedTeam').required = true;
+        document.getElementById('vacationDaysTotal').required = true;
+        document.getElementById('vacationDaysLeft').required = true; 
+    }
+    
+}
 var isNewAccount = false;
 function createProfile() {
     isNewAccount = true;
