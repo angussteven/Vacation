7f742a53f447f8e4eb7292b2154b68fc6521d2c8
BRETT <BRETTREARDON123@GMAIL.COM>
2016-08-19 08:09:51 -0700

    Added favicon, sidebar changes for changing team (nonfunctional)

--- js/DBConfig.js
+++ js/DBConfig.js
@@ -24,7 +24,6 @@ var employeeEvents;
 
 firebase.auth().onAuthStateChanged(function (user) {
 	profileEmail = user.email;
-	renderEmployeeEvents(profileEmail);
 
 	// // Implementation of getEmployeeCount
 	// var getEmpCountTest = $.Deferred(getEmployeeCount);
@@ -76,10 +75,7 @@ firebase.auth().onAuthStateChanged(function (user) {
 		//console.log(sessionStorage.getItem("image"));
 		getProfileImage();
 	});
-	getHolidays().then(function (snap) {
-		//console.log(snap.val());
-		sessionStorage.setItem('holidays', JSON.stringify(snap.val()));
-	});
+
 
 
 	// // Implementation of getTeam
@@ -468,21 +464,7 @@ function getImage(fileName) {
 
 	//console.log(path);
 }
-//getHolidays();
-function getHolidays(){
-			return new Promise(function (resolve, reject) {
-			var ref = firebase.database().ref().child('holiday').child('day');
-			ref.once('value', function (snapshot) {
-				if (snapshot.exists()) {
-					resolve(snapshot)
-				}
-				else {
-					reject(reject);
-				}
-			})
-		})
 
-}
 
 function updateDaysLeft(email, daysLeft) {
 	var tempEmail = fixEmail(email);
