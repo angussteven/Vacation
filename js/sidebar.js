be8dc169f6a48c7ea730ebafaca44d3a05095db0
Luis Valenzuela <FZ3T8L@NAM.corp.gm.com>
2016-09-13 10:27:45 -0700

    Sidebar toggle button padded, div size resizing on sidebar expand

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,17 +1,27 @@
+var menuOpen = false;
+
 function toggleSideBar(burgerBar) {
     var sidebar = document.getElementById("sidebar");
     var sidebarContainer = document.getElementById("sidebarContainer");
+    var calendarContainer = document.getElementById("wrap");
     var width = sidebarContainer.offsetWidth;
     
     console.log("Menu width: " + width);
     if (width <= 0) {
         sidebarContainer.style.width = "100%";
-        sidebar.style.width = "20%";
+        sidebar.style.width = "25em";
+        calendarContainer.style.width = "70%";
+        //sidebar.style.backgroundColor = "#80b3ff"
+        sidebar.style.backgroundImage = "url('img/background.png')";
+        menuOpen = true;
         $("#sidebarContainer").show();
     }
     else {
         sidebarContainer.style.width = "0";
         sidebar.style.width = "0";
+        calendarContainer.style.width = "95%";
+        sidebar.style.backgroundImage = "none";
+        menuOpen = false;
         $("#sidebarContainer").hide();
     }
     burgerBar.classList.toggle("animate");
