5f03c1fd988d5cc13385d282fb8295b3368366e8
Luis Valenzuela <FZ3T8L@NAM.corp.gm.com>
2016-09-14 10:19:36 -0700

    Added div that is toggle by settings icon.

---
+++ js/menucontroller.js
@@ -0,0 +1,42 @@
+var sideBarOpen = true;
+var settingsMenuOpen = false;
+
+function toggleSideBar(burgerBar) {
+    var sidebar = document.getElementById("sidebar");
+    var sidebarContainer = document.getElementById("sidebarContainer");
+    var calendarContainer = document.getElementById("wrap");
+    var width = sidebarContainer.offsetWidth;
+    
+    console.log("Menu width: " + width);
+    
+    if (sideBarOpen) {
+        sidebar.style.left = "500px";
+        sideBarOpen = false;
+    }
+    else {
+        sidebar.style.left = "0";
+        sideBarOpen = true;
+    }
+    animateButton(burgerBar);
+}
+
+function toggleProfileSettings(settings) {
+    var settingsMenu = document.getElementById("profileSettings");
+    
+    if (settingsMenuOpen) {
+        console.log("Moved menu to the left");
+        settingsMenu.style.right = "-800px";
+        settingsMenuOpen = false;
+    }
+    else {
+        console.log("Moved menu to the right");
+
+        settingsMenu.style.right = "0";
+        settingsMenuOpen = true;
+    }
+    
+}
+
+function animateButton(burgerBar) {
+    burgerBar.classList.toggle("animate");
+}
