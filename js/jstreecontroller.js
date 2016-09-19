b28f475228e8ee2e0b8c886615e27f60ec473907
Luis Valenzuela <FZ3T8L@NAM.corp.gm.com>
2016-09-19 15:08:12 -0700

    Made arrow in tree drop down without selecting node.

--- js/jstreecontroller.js
+++ js/jstreecontroller.js
@@ -1,170 +1,169 @@
 /*
-* Keeps track the events that are currently visible on the calendar.
-*/
+ * Keeps track the events that are currently visible on the calendar.
+ */
 var activeEvents = [];
 var activeEmployees = [];
 
 function test(teamData) {
-	$('#container').on('click', 'li', function (e) {
-		e.stopPropagation();
-		var bgID = $(this).css("background-color");
-		var manager;
-		var employees;
+    $('#container').on('click', 'a', function(e) {
+        e.stopPropagation();
+        var bgID = $(this).css("background-color");
+        var manager;
+        var employees;
 
-		if ($.inArray(this.id.toString(), activeEmployees) != -1) {
-			/*
+        if ($.inArray(this.parentNode.id.toString(), activeEmployees) != -1) {
+            /*
 			* If the manager is selected then remove all events from the calendar.
 
 			if ($(this).attr('class').indexOf('manager') > -1) {
-				manager = document.getElementById(this.id);
+				manager = document.getElementById(this.parentNode.id);
 				employees = manager.getElementsByTagName("li");
 				for (var i = 0; i < employees.length; i++) {
 					removeEmployeeEvents(teamData[employees[i].id].email);
 				}
 			}
             */
-            document.getElementById(this.id.toString()).style.color = "#ffffff";
-			activeEmployees.splice(activeEmployees.indexOf(this.id.toString(), 1));
-			removeEmployeeEvents(teamData[this.id].email);
-		}
-		else {
-			/*
+            document.getElementById(this.parentNode.id.toString()).style.color = "#ffffff";
+            activeEmployees.splice(activeEmployees.indexOf(this.parentNode.id.toString(), 0));
+            removeEmployeeEvents(teamData[this.parentNode.id].email);
+        } else {
+            /*
 			* If the manager is selected populate the calendar with everyones events.
 
 			if ($(this).attr('class').indexOf('manager') > -1) {
-				manager = document.getElementById(this.id);
+				manager = document.getElementById(this.parentNode.id);
 				employees = manager.getElementsByTagName("li");
 				for (var i = 0; i < employees.length; i++) {
 					renderEmployeeEvents(teamData[employees[i].id].email);
 				}
 			}
             */
-            document.getElementById(this.id.toString()).style.color = "#76a1cf";
-			activeEmployees.push(this.id.toString());
-			renderEmployeeEvents(teamData[this.id].email);
-		}
+            document.getElementById(this.parentNode.id.toString()).style.color = "#76a1cf";
+            activeEmployees.push(this.parentNode.id.toString());
+            renderEmployeeEvents(teamData[this.parentNode.id].email);
+        }
         /*
-        console.log("Employee Selected: " + this.id.toString());
+        console.log("Employee Selected: " + this.parentNode.id.toString());
         cofnsole.log("Active Employees: ");
         console.log(activeEmployees);
         console.log("Active Events: ");
         console.log(activeEvents);
         */
-	});
+    });
 }
 
 /*
-* This function removed the events from a calendar belonging to the employee that was deselected.
-* It takes that persons employeeID as an argument.
-*/
+ * This function removed the events from a calendar belonging to the employee that was deselected.
+ * It takes that persons employeeID as an argument.
+ */
 function removeEmployeeEvents(employeeID) {
-	// Local Variables
-	var promise;
-	var event;
-	/*
-	* The promise is used to make sure that we have data back from the database before we continue
-	* with the function.
-	*/
-	promise = getEmployeeEvents(employeeID.toString()).then(function (data) {
-		/*
-		* We iterate over the events belonging to an employee.
-		*/
-		for (var currentEvent in data) {
-			/*
-			* These events are then checked against the local array and if its there it is removed.
-			*/
-			if ($.inArray(data[currentEvent].eventID.toString(), activeEvents) != -1) {
-				activeEvents.splice(activeEvents.indexOf(data[currentEvent].eventID.toString()), 1);
-				$('#calendar').fullCalendar('removeEvents', data[currentEvent].eventID.toString());
-			}
-		}
-	}).catch(function (error) {
-		console.log(error);
-	});
+    // Local Variables
+    var promise;
+    var event;
+    /*
+     * The promise is used to make sure that we have data back from the database before we continue
+     * with the function.
+     */
+    promise = getEmployeeEvents(employeeID.toString()).then(function(data) {
+        /*
+         * We iterate over the events belonging to an employee.
+         */
+        for (var currentEvent in data) {
+            /*
+             * These events are then checked against the local array and if its there it is removed.
+             */
+            if ($.inArray(data[currentEvent].eventID.toString(), activeEvents) != -1) {
+                activeEvents.splice(activeEvents.indexOf(data[currentEvent].eventID.toString()), 1);
+                $('#calendar').fullCalendar('removeEvents', data[currentEvent].eventID.toString());
+            }
+        }
+    }).catch(function(error) {
+        console.log(error);
+    });
 }
 
 /*
-* This function displays all of the events belonging to a given employee. It takes the employeeID
-* as an argument.
-*/
+ * This function displays all of the events belonging to a given employee. It takes the employeeID
+ * as an argument.
+ */
 function renderEmployeeEvents(employeeID) {
-	// Local variables.
-	var promise;
-	var event;
-	var dataResult;
-	/*
-	* The promise is used to make sure that we have data back from the database before we continue
-	* with the function.
-	*/
-	getEmployeeEvents(employeeID.toString()).then(function (data) {
-		dataResult = JSON.stringify(data);
-		realData = JSON.parse(dataResult);
-		/*
-		* Every event returned from the database is added into our local array.
-		*/
-		for (var currentEvent in realData) {
-			/*
-			* If the event is already in the array we don't save it again.
-			*/
-			if ($.inArray(realData[currentEvent].eventID.toString(), activeEvents) == -1) {
-				/*
-				* The event is saved as a key-pair value array.
-				*/
-				event = {
-					owner: employeeID,
-					id: realData[currentEvent].eventID,
-					title: realData[currentEvent].title,
-					start: realData[currentEvent].startDate,
-					end: realData[currentEvent].endDate,
-					description: realData[currentEvent].description
-				};
-				/*
-				* We use the eventID to keep track of currently rendered events so that is
-				* saved into the array.
-				*/
-				activeEvents.push(realData[currentEvent].eventID.toString());
-				/*
-				* The current event is then rendered using the information in the event
-				* variable.
-				*/
-				$('#calendar').fullCalendar('renderEvent', event, true);
-			}
+    // Local variables.
+    var promise;
+    var event;
+    var dataResult;
+    /*
+     * The promise is used to make sure that we have data back from the database before we continue
+     * with the function.
+     */
+    getEmployeeEvents(employeeID.toString()).then(function(data) {
+        dataResult = JSON.stringify(data);
+        realData = JSON.parse(dataResult);
+        /*
+         * Every event returned from the database is added into our local array.
+         */
+        for (var currentEvent in realData) {
+            /*
+             * If the event is already in the array we don't save it again.
+             */
+            if ($.inArray(realData[currentEvent].eventID.toString(), activeEvents) == -1) {
+                /*
+                 * The event is saved as a key-pair value array.
+                 */
+                event = {
+                    owner: employeeID,
+                    id: realData[currentEvent].eventID,
+                    title: realData[currentEvent].title,
+                    start: realData[currentEvent].startDate,
+                    end: realData[currentEvent].endDate,
+                    description: realData[currentEvent].description
+                };
+                /*
+                 * We use the eventID to keep track of currently rendered events so that is
+                 * saved into the array.
+                 */
+                activeEvents.push(realData[currentEvent].eventID.toString());
+                /*
+                 * The current event is then rendered using the information in the event
+                 * variable.
+                 */
+                $('#calendar').fullCalendar('renderEvent', event, true);
+            }
 
-		}
-        activeEmployees.push(fixEmail(employeeID.toString()));
-	}).catch(function (data) {
-		console.log("No events found for: " + employeeID);
-	});
+        }
+    }).catch(function(data) {
+        console.log("No events found for: " + employeeID);
+    });
+		activeEmployees.push(fixEmail(employeeID.toString()));
 }
 
 /*
-* This function takes json data as an argument and uses that data to populate the tree dynamically.
-*/
+ * This function takes json data as an argument and uses that data to populate the tree dynamically.
+ */
 function populateList(teamData) {
-	/*
-	* These vaiables hold the different HTML objects that we insert into the HTML file.
-	*/
-	var unorderedList = document.createElement('ul');
-	var manager;
+    /*
+     * These vaiables hold the different HTML objects that we insert into the HTML file.
+     */
+    var unorderedList = document.createElement('ul');
+    var manager;
     var managerNode;
-	var employee;
-	var nestedList;
-	/*
-	* The first for loop gets the manager and addes it as a child of the container div.
-	*/
-	for (var key in teamData) {
-		if(teamData[key].isManager) {
+    var employee;
+    var nestedList;
+    /*
+     * The first for loop gets the manager and addes it as a child of the container div.
+     */
+    for (var key in teamData) {
+        if (teamData[key].isManager) {
             manager = document.createElement('li');
-			manager.appendChild(document.createTextNode(capitalize(teamData[key].firstName) + " " + capitalize(teamData[key].lastName)));
-			manager.setAttribute('id', key.toString());
-			manager.setAttribute('class', 'manager');
+            manager.appendChild(document.createTextNode(capitalize(teamData[key].firstName) + " " + capitalize(teamData[key].lastName)));
+            manager.setAttribute('id', key.toString());
+            manager.setAttribute('class', 'manager');
             nestedList = document.createElement('ul');
             /*
-            * This second for loop creates an HTML element for every team member which are then added
-            * as a child of the manager.
-            */
+             * This second for loop creates an HTML element for every team member which are then added
+             * as a child of the manager.
+             */
             for (var person in teamData) {
-                if (!teamData[person].isManager &&  teamData[person].managers == teamData[key].email) { //$.inArray(teamData[key].employees, != -1)) {
+                if (!teamData[person].isManager && teamData[person].managers == teamData[key].email) { //$.inArray(teamData[key].employees, != -1)) {
                     employee = document.createElement('li');
                     employee.appendChild(document.createTextNode(capitalize(teamData[person].firstName) + " " + capitalize(teamData[person].lastName)));
                     employee.setAttribute('id', person.toString());
@@ -172,26 +171,25 @@ function populateList(teamData) {
                 }
             }
             /*
-            * The current is then added as a child to thye root unorderded list.
-            */
+             * The current is then added as a child to thye root unorderded list.
+             */
             unorderedList.appendChild(manager);
-	        manager.appendChild(nestedList);
-		}
-	}
-	/*
-	* The unordered list is added as a child of the container div.
-	*/
-	document.getElementById("container").appendChild(unorderedList);
+            manager.appendChild(nestedList);
+        }
+    }
+    /*
+     * The unordered list is added as a child of the container div.
+     */
+    document.getElementById("container").appendChild(unorderedList);
 }
 
 /*
-* This function capilizes the first letter of the string passed in.
-*/
+ * This function capilizes the first letter of the string passed in.
+ */
 function capitalize(string) {
-	try{
-		return string[0].toUpperCase() + string.slice(1);
-	}
-    catch(error){
-		return "Returned Null";
-	}
+    try {
+        return string[0].toUpperCase() + string.slice(1);
+    } catch (error) {
+        return "Returned Null";
+    }
 }
